4d6c018fcc170007e9e740c34d5fd456452256ad
Bo3866 <werther0422@gmail.com>
2026-06-07 00:54:05 +0800

    寄信修改

--- manual_remind.php
+++ manual_remind.php
@@ -10,7 +10,8 @@ require_once __DIR__ . '/auto_remind.php';
 
 try {
     if (function_exists('run_auto_remind')) {
-        $res = run_auto_remind(true);
+        // force=false：受冷卻限制，老師按鈕也要等1小時才能再寄
+        $res = run_auto_remind(false);
         echo json_encode([
             'ok'    => true,
             'sent'  => (int)($res['sent'] ?? 0),
